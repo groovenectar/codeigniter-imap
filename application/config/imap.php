bcd7b02226cb2b72cbfdbfcd2874ac255309b34d
Daniel Upshaw <contact@danielupshaw.com>
2017-03-22 17:26:50 -0400

    Spacing

--- application/config/imap.php
+++ application/config/imap.php
@@ -1,6 +1,6 @@
 <?php
 
-$config['imap_host']     ='imap-mail.outlook.com';
-$config['imap_encrypto'] ='ssl';
-$config['imap_user']     ='phpimapclient@outlook.com';
-$config['imap_pass']     ='Abcd12345**';
+$config['imap_host']     = 'imap-mail.outlook.com';
+$config['imap_encrypto'] = 'ssl';
+$config['imap_user']     = 'phpimapclient@outlook.com';
+$config['imap_pass']     = 'Abcd12345**';
